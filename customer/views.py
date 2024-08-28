65f97f5beac54eb5989818a5709a1c4994df6b77
asirff399 <aintisar48@gmail.com>
2024-08-28 10:51:53 +0600

    login link correction

--- customer/views.py
+++ customer/views.py
@@ -60,7 +60,7 @@ class UserRegistrationApiView(APIView):
             print('token', token)
             uid = urlsafe_base64_encode(force_bytes(user.pk))
             print('uid',uid)
-            confirm_link = f'http://127.0.0.1:8000/customer/active/{uid}/{token}'
+            confirm_link = f'https://exipet-drf-api.onrender.com/customer/active/{uid}/{token}'
             email_subject = "Confirmation Email"
             email_body = render_to_string('confirm_email.html',{'confirm_link':confirm_link}) 
             email = EmailMultiAlternatives(email_subject,'',to=[user.email])
